44e9da4e8699c7fa540390e09c7c3fdb68acd7d0
Halden <haldenk@razoyo.com>
2025-02-10 10:46:34 -0700

    handle draw, reset button

--- lib/tic_tac_toe_web/live/tic_tac_toe_live.ex
+++ lib/tic_tac_toe_web/live/tic_tac_toe_live.ex
@@ -13,7 +13,7 @@ defmodule TicTacToeWeb.TicTacToeLive do
     IO.inspect(index, label: "index of button clicked")
     # converts the index of the tile to an integer
     index = String.to_integer(index)
-    #the board, from assign
+    # the board, from assign
     board = socket.assigns.board
     # the turn, from assign
     turn = socket.assigns.turn
@@ -36,68 +36,97 @@ defmodule TicTacToeWeb.TicTacToeLive do
       changeset = TicTacToe.Game.changeset(game, updated_game)
       # push changes to the DB
       TicTacToe.Repo.insert_or_update(changeset)
-
+      # after a move is made, check if there is a winner, draw, or if the game should keep going
       case check_winner(updated_game) do
+        # if winner, reset board and update winner list
         {:winner, winner, updated_game} ->
           {:noreply, assign(socket, board: List.duplicate("", 9), turn: "X", x_wins: updated_game.x_wins, o_wins: updated_game.o_wins)}
+        # if draw, keep win count, and reset board
+        {:draw, updated_game} ->
+          {:noreply, assign(socket, board: List.duplicate("", 9), turn: "X", x_wins: updated_game.x_wins, o_wins: updated_game.o_wins)}
+        # if no winner, and no draw, continue playing
         :no_winner ->
           {:noreply, assign(socket, board: updated_board, turn: next_turn)}
       end
-
     else
       {:noreply, socket}
     end
   end
 
   def check_winner(updated_game) do
+    # all winning combinations
     winning_combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    # fetch the latest game state from DB
     game = TicTacToe.Repo.one(TicTacToe.Game) || %TicTacToe.Game{x_moves: [], o_moves: [], x_wins: 0, o_wins: 0}
+    # check stored moves against winning combinations to see if there is a winner
     winner =
       cond do
+        # check against x moves
         Enum.any?(winning_combinations, fn combo -> Enum.all?(combo, &(&1 in game.x_moves)) end) -> "X"
+        # check against o moves
         Enum.any?(winning_combinations, fn combo -> Enum.all?(combo, &(&1 in game.o_moves)) end) -> "O"
         true -> nil
       end
+      # handle result based on winner
       case winner do
+        # if x wins, reset the board and update x_wins
         "X" ->
           updated_game = %{x_moves: [], o_moves: [], x_wins: game.x_wins + 1, o_wins: game.o_wins}
           changeset = TicTacToe.Game.changeset(game, updated_game)
           TicTacToe.Repo.insert_or_update(changeset)
           {:winner, "X", updated_game}
+        # if o wins, reset board, and update o_wins
         "O" ->
           updated_game = %{x_moves: [], o_moves: [], x_wins: game.x_wins, o_wins: game.o_wins + 1}
           changeset = TicTacToe.Game.changeset(game, updated_game)
           TicTacToe.Repo.insert_or_update(changeset)
           {:winner, "O", updated_game}
+        # if no winner
         nil ->
-          :no_winner
+          # get number of moves
+          all_moves = game.x_moves ++ game.o_moves
+          # if the number of moves == the total number of tiles, reset the board
+          if length(all_moves) == 9 do
+            updated_game = %{x_moves: [], o_moves: [], x_wins: game.x_wins, o_wins: game.o_wins}
+            changeset = TicTacToe.Game.changeset(game, updated_game)
+            TicTacToe.Repo.insert_or_update(changeset)
+            {:draw, updated_game}
+          else
+            # continue the game if there is no winner and not all of the tiles are filled
+            :no_winner
+          end
       end
   end
 
-
-
-  # need check winner func, wich will check both players coordinates against the winning combinations. Called in the move handle event
-    # If winner, increment o_wins, or x_wins. Call clear board function. Clear x_moves and o_moves array in DB
-    # if no winner, return
-
-  # need clear board func, to be called when someone wins
-
-  # need reset score function
-
+  def handle_event("reset", _params, socket) do
+    # fetch latest game state from DB
+    game = TicTacToe.Repo.one(TicTacToe.Game) || %TicTacToe.Game{x_moves: [], o_moves: [], x_wins: 0, o_wins: 0}
+    # reset all DB tables to create updated game
+    updated_game = %{x_moves: [], o_moves: [], x_wins: 0, o_wins: 0}
+    # create changeset and update the DB
+    changeset = TicTacToe.Game.changeset(game, updated_game)
+    TicTacToe.Repo.insert_or_update(changeset)
+    # update assigns with new game
+    {:noreply, assign(socket, board: List.duplicate("", 9), turn: "X", x_wins: 0, o_wins: 0)}
+  end
   def render(assigns) do
     ~H"""
     <div class="flex flex-col items-center justify-center">
       <h2 class="text-3xl text-center mb-3">Welcome to tic tac toe!</h2>
       <h3 class="text-xl">Current turn: <%= @turn %> </h3>
+      <div class="flex mt-2 space-x-2">
+        <p class="border border-gray-200 rounded-md p-2 "> X wins: <%= @x_wins %></p>
+        <p class="border border-gray-200 rounded-md p-2"> O wins: <%= @o_wins %></p>
+      </div>
       <div class="grid grid-cols-3 mt-5">
         <%= for {value, index} <- Enum.with_index(@board) do %>
-        <button phx-click="move" phx-value-index={index} class="border border-black h-20 w-20">
+        <button phx-click="move" phx-value-index={index} class="border border-black h-20 w-20 hover:bg-gray-200">
           <%= value %>
         </button>
         <% end %>
-        <p> x wins: <%= @x_wins %></p>
-        <p> y wins: <%= @o_wins %></p>
       </div>
+
+      <button phx-click="reset" class="mt-2 border border-gray-200 rounded-md hover:bg-red-100 p-2" > Reset Game </button>
     </div>
     """
   end
